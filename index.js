84c2bdda23ea5d7b2fdc57afb27c3907eed5a91c
marshall405 <marshall.slemp@gmail.com>
2020-06-09 12:46:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+
+function map(arr, callback) {
+    const returnArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        returnArr.push(callback(arr[i]))
+    }
+    return returnArr
+}
+
+function reduce(arr, callback, accum) {
+    let i = 0;
+    if (!accum) {
+        accum = arr[0]
+        i = 1
+    }
+    for (; i < arr.length; i++) {
+        accum = callback(arr[i], accum)
+    }
+    return accum
+}
