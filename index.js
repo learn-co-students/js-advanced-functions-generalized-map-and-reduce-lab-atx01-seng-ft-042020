1aa4a01044c58889049d930d1d6a8bf156254727
iambryanhaney <i.am.bryan.haney@gmail.com>
2020-06-22 17:34:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+function map(arr, func) {
+    let newarr = []
+    for(let i = 0; i < arr.length; i++) {
+        newarr.push(func(arr[i]))
+    }
+    return newarr
+}
+
+function reduce(arr, func, accumulator) {
+    let newarr = []
+    let i = 0;
+    if (accumulator === undefined) {
+        accumulator = arr[0]
+        i = 1
+    }
+    for(i; i < arr.length; i++) {
+        accumulator = func(arr[i], accumulator)
+    }
+    return accumulator
+}
