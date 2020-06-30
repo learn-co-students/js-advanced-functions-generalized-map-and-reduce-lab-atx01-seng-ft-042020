4a2485c25ff6e3c6edb76f06dac8fdf3fbc18b36
rhinehardtc <rhinehardtc@gmail.com>
2020-06-30 00:18:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Add your functions here
+function map(sourceArray, block){
+    const newArray = [];
+
+    for(let i=0; i<sourceArray.length; i++){
+        newArray.push(block(sourceArray[i]))
+    }
+    return newArray;
+};
+
+
+
+function reduce(sourceArray, block, start){
+    let starter = (!!start) ? start : sourceArray[0];
+    
+    for( let i = (!!start) ? 0 : 1; i<sourceArray.length; i++){
+        starter = block(sourceArray[i], starter);
+    };
+
+    return starter;
+}
